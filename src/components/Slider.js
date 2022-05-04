4ee7f604291d564c3b73473b1e1fb2b23cc50285
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-03 23:48:24 -0600

    Update first image

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,7 +4,7 @@ import {data} from './data'
 import "./Slider.css"
 
 const Slider = () => {
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(2)
   return (
     <div className='slider'>
       <BsFillArrowLeftCircleFill className="arrow prev" />
@@ -13,7 +13,17 @@ const Slider = () => {
       {data.map((slide, index) => {
         return (
           <div className={index === currentSlide ? 'slide current' : 'slide'} key={index}>
-            
+            {index === currentSlide && (
+              <>
+              <img src={slide.image} alt={slide.heading} />
+              <div className="content">
+                <h2>{slide.heading}</h2>
+                <p>{slide.desc}</p>
+                <hr />
+                <button className="--btn --btn-primary">Get Started</button>
+              </div>
+              </>
+            )}
           </div>
         )
       })}
--- src/components/data.js
+++ src/components/data.js
@@ -1,13 +1,17 @@
 export const data = [
   {
-    image: "../../public/images/main2.jpeg", 
+    image: './images/main2.jpeg', 
     heading: "Calgary Best Hamburger",
     desc: "This is good",
   },
   {
-    image: "../../public/images/main2.jpeg", 
-    heading: "Calgary Best Hamburger",
-    desc: "This is good",
+    image: './images/main1.jpeg', 
+    heading: "Best Milkshake",
+    desc: "This is good2",
   },
-
+  {
+    image: './images/main3.jpeg', 
+    heading: "Deep fried pickle",
+    desc: "Our new menu",
+  },
 ]
