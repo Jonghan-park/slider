06e23469475a7cec7f50e3ddd2cda2af4b5203a2
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-04 00:33:51 -0600

    Update moving the slide

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Button from 'bootstrap/js/dist/button'
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill} from 'react-icons/bs'
 import {data} from './data'
 import "./Slider.css"
 
 const Slider = () => {
-  const [currentSlide, setCurrentSlide] = useState(2)
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const slideLength = data.length
+  // slideLength = 1 2 3
+  // crrentSlide = 0 1 2
+  const nextSlide =() =>{
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1)
+  }
+
+  const prevSlide =() =>{
+    setCurrentSlide(currentSlide === 0 ? slideLength -1 : currentSlide - 1)
+  }
+
+  useEffect(() =>{
+    setCurrentSlide(0)
+  },[])
+
   return (
     <div className='slider'>
-      <BsFillArrowLeftCircleFill className="arrow prev" />
-      <BsFillArrowRightCircleFill className="arrow next" />
+      <BsFillArrowLeftCircleFill className="arrow prev" onClick={prevSlide} />
+      <BsFillArrowRightCircleFill className="arrow next" onClick={nextSlide} />
       
       {data.map((slide, index) => {
         return (
@@ -17,7 +33,7 @@ const Slider = () => {
               <>
               <img src={slide.image} alt={slide.heading} />
               <div className="content">
-                <h2>{slide.heading}</h2>
+                <h1>{slide.heading}</h1>
                 <p>{slide.desc}</p>
                 <hr />
                 <button className="--btn --btn-primary">Get Started</button>
