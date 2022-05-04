099a074eb32eeee05e069eaea1fb186e1d80d052
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-03 23:22:41 -0600

    Install icons

--- src/components/data.js
+++ src/components/data.js
@@ -4,5 +4,10 @@ export const data = [
     heading: "Calgary Best Hamburger",
     desc: "This is good",
   },
+  {
+    image: "../../public/images/main2.jpeg", 
+    heading: "Calgary Best Hamburger",
+    desc: "This is good",
+  },
 
 ]
